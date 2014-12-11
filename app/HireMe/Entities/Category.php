03b20dba47b0cbc38b954bb89ce9499c9128bdeb
SergioGregorutti <sagregorutti@gmail.com>
2014-12-11 20:11:09 -0300

    Creacion de vista incluyendo datos dinámicos

--- app/HireMe/Entities/Category.php
+++ app/HireMe/Entities/Category.php
@@ -3,4 +3,9 @@ namespace HireMe\Entities;
 
 class Category extends \Eloquent {
 	protected $fillable = [];
+
+    public function candidates()
+    {
+        return $this->hasMany('HireMe\Entities\Candidate');
+    }
 }
